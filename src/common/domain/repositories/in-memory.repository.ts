c7a81aee05f55082e8b84cacff1617d529305aff
MatheusTrinca <matheusfelipetrinca@gmail.com>
2025-03-23 17:02:50 +0000

    test: in-memory-repository-test

--- src/common/domain/repositories/in-memory.repository.ts
+++ src/common/domain/repositories/in-memory.repository.ts
@@ -53,7 +53,60 @@ export abstract class InMemoryRepository<Model extends ModelProps>
   }
 
   async search(props: SearchInput): Promise<SearchOutput<Model>> {
-    throw new Error('Method not implemented.')
+    const page = props.page ?? 1
+    const per_page = props.per_page ?? 15
+    const sort = props.sort ?? null
+    const filter = props.filter ?? null
+    const sort_dir = props.sort_dir ?? null
+
+    const filteredItems = await this.applyFilter(this.items, filter)
+    const sortedItems = await this.applySort(filteredItems, sort, sort_dir)
+    const paginatedItems = await this.applyPaginate(sortedItems, page, per_page)
+
+    return {
+      items: paginatedItems,
+      total: filteredItems.length,
+      current_page: page,
+      per_page,
+      sort,
+      sort_dir,
+      filter,
+    }
+  }
+
+  protected abstract applyFilter(
+    items: Model[],
+    filter: string | null,
+  ): Promise<Model[]>
+
+  protected async applySort(
+    items: Model[],
+    sort: string | null,
+    sort_dir: string | null,
+  ): Promise<Model[]> {
+    if (!sort || !this.sortableFields.includes(sort)) {
+      return items
+    }
+
+    return [...items].sort((a, b) => {
+      if (a[sort] < b[sort]) {
+        return sort_dir === 'asc' ? -1 : 1
+      }
+      if (a[sort] > b[sort]) {
+        return sort_dir === 'asc' ? 1 : -1
+      }
+      return 0
+    })
+  }
+
+  protected async applyPaginate(
+    items: Model[],
+    page: number,
+    per_page: number,
+  ): Promise<Model[]> {
+    const start = (page - 1) * per_page
+    const limit = start + per_page
+    return items.slice(start, limit)
   }
 
   protected async _get(id: string): Promise<Model> {
